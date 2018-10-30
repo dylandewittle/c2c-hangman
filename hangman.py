8b2d683b9f2332b511a9d9146210e330e197030a
Dylan Le <dylandewittle@4261811f904c>
2018-10-30 22:26:03 +0000

    Changes to the hangman code

--- hangman.py
+++ hangman.py
@@ -45,3 +45,27 @@ def draw_hangman(num_wrong_guesses):
         output = output + "\n |"
     output = output + "____\n\n"
     print(output)
+
+#check a guess to see if it is correct
+
+#get input from console
+name = input("What is your name?")
+print("Hello",name,"! Time to play Hangman!")
+word = 'test'
+
+wrong_guesses = 0
+
+
+while wrong_guesses < num_wrong_guesses_allowed:
+    draw_hangman(wrong_guesses)
+    guess = input("Guess the letter!").lower()
+    if guess in word:
+        print("Correct!")
+    else:
+        print("Incorrect.")
+        wrong_guesses = wrong_guesses + 1
+
+
+
+
+
